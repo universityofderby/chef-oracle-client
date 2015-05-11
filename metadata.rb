b3be29e32ec5cdb333c11277c37c918bbcb46786
Damacus <dan.m.webb@gmail.com>
2015-05-11 16:54:05 +0100

    update version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ai@derby.ac.uk'
 license 'Apache 2.0'
 description 'Installs/Configures oracle-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
+version '0.1.0' 
 
 depends 'ark'
 depends 'resource'
