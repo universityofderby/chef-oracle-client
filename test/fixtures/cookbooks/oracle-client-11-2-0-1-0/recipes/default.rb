75fcea06aa58880e27418ef68030adb08628822b
Daniel Webb <dan.m.webb@gmail.com>
2015-11-28 10:49:02 +0000

    ammend installer zip name to match OTN

--- test/fixtures/cookbooks/oracle-client-11-2-0-1-0/recipes/default.rb
+++ test/fixtures/cookbooks/oracle-client-11-2-0-1-0/recipes/default.rb
@@ -5,5 +5,5 @@ node.default['oracle']['client']['tnsnames']['ktst']['service_name'] = 'ktst'
 oracle_client '11.2.0.1' do
   ownername 'oracle'
   groupname 'oracle'
-  installer_file 'client-11.2.0.1-installer.zip'
+  installer_file 'linux.x64_11gR2_client.zip'
 end
