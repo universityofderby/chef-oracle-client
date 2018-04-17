a33261e0a89a829f2020c514284f4b2d4c88a941
sumitag <sumit.aga@gmail.com>
2018-04-17 08:43:50 -0400

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ai@derby.ac.uk'
 license 'Apache 2.0'
 description 'Installs/Configures oracle-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.1'
+version '0.3.2'
 source_url 'https://github.com/sumitag/chef-oracle-client/tree/rhel7_12.2'
 issues_url 'https://github.com/universityofderby/chef-oracle-client/issues'
 
